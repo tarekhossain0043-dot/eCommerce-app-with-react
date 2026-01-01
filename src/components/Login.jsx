04a07705d90ecfc6441b0f9d76daceb9ec2df858
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2026-01-01 10:38:25 +0600

    tailwind v3 to upgrade v4

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+
 export default function Login() {
   return (
     <div className="flex items-center justify-center bg-white h-screen mt-12">
       <div className="max-w-[540px] w-full shadow-sm shadow-slate-300 rounded-sm bg-white p-12">
-        <h4 className="text-center">Create an Account</h4>
+        <h3 className="text-center mb-2">Create an Account</h3>
+        <p>Have an Account?</p>
+        
       </div>
     </div>
   );
