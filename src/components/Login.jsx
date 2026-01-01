2f51b8372c6bde4e8a0e2bdeaa5dbfff17ab52b7
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2026-01-01 10:20:39 +0600

    login components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="flex items-center justify-center bg-white h-screen mt-12">
+      <div className="max-w-[540px] w-full shadow-sm shadow-slate-300 rounded-sm bg-white p-12">
+        <h4 className="text-center">Create an Account</h4>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "350px",
+      sm: "600px",
+      md: "767px",
+      lg: "1024px",
+      xl: "1220px",
+    },
+    colors: {
+      white: "#FFF",
+      "gray-600": "#5A607F",
+      blue: "#1E5EFF",
+      "slate-400": "#D7DBEC",
+      "gray-400": "#A1A7C4",
+      "slate-300": "#D9E1EC",
+      black: "#131523",
+      "indigo-500": "#1877F2",
+      "indigo-600": "#1976D2",
+      "jam-clr": "#1E2753",
+      "green-clr": "#4CAF50",
+      "pink-clr": "#FF3D00",
+      "yellow-clr": "#FFC107",
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
