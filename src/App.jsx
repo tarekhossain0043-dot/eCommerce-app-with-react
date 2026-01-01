f3ee77f01b26286b3891fbc8f6577f2b87057942
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2026-01-01 13:11:59 +0600

    feature/firebase setup for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ProtectedRoute from "./user-login-status/ProtectedRoute";
 import DashboardLayout from "./user-login-status/DashboardLayout";
 import DashboardHome from "./dashboard-inner-component/DashboardHome";
 import Reset_password from "./components/Reset_password";
+import Comfirmation_with_mail from "./components/Comfirmation_with_mail";
 
 export default function App() {
   return (
@@ -15,6 +16,10 @@ export default function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/login" element={<Login />} />
         <Route path="/reset-password" element={<Reset_password />} />
+        <Route
+          path="/confirmation_with_mail"
+          element={<Comfirmation_with_mail />}
+        />
         {/* protected routes for user login or not */}
         <Route element={<ProtectedRoute />}>
           <Route element={<DashboardLayout />}>
--- src/components/Comfirmation_with_mail.jsx
+++ src/components/Comfirmation_with_mail.jsx
@@ -1,43 +1,47 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export default function Reset_password() {
+export default function Comfirmation_with_mail() {
   const navigate = useNavigate();
   return (
     <div className="flex items-center justify-center h-screen bg-white my-10">
       <div className="max-w-135 w-full p-12 rounded-sm bg-white shadow-sm">
         <div className="mb-10">
-          <h3 className="mb-2 text-center">Password Reset</h3>
-          <p className="text-center">We Will Help You Reset your Password</p>
+          <h3 className="mb-2 text-center">Confirm Email</h3>
+          <p className="text-center">
+            Check Your Email and Enter Confirmation Code
+          </p>
         </div>
         <form className="w-full flex flex-col gap-6">
           <div className="flex flex-col gap-1">
-            <label htmlFor="email" className="text-default">
-              Email
+            <label htmlFor="number" className="text-default">
+              Confirmation Code
             </label>
             <input
-              type="email"
-              placeholder="Enter Email Address"
-              id="email"
-              name="email"
+              type="number"
+              placeholder="Enter Code"
+              id="number"
+              name="number"
+              maxLength={6}
+              minLength={6}
               className="text-gray-700 capitalize px-4 py-3 rounded-sm ring-1 ring-slate-100 outline-none w-full focus:ring-blue-2"
             />
           </div>
 
           <input
             type="submit"
-            onClick={() => navigate("/confirmation_with_mail")}
             className="w-full py-3 bg-primary text-center text-white text-[16px] leading-6 rounded-sm cursor-pointer transition-all duration-500 ease-in-out hover:bg-blue-3 hover:text-primary"
-            value="Reset Password"
+            value="Confirm Email"
           />
+          <hr className="bg-slate-100 w-full h-px" />
           {/* <div className="flex flex-col gap-1 text-center mb-6 divide-solid divide-slate-100"></div> */}
-          <p className="mb-6 text-center">Remembered your Password? </p>
+          <p className="mb-6 text-center">Haven’t received your code?</p>
           <div
-            onClick={() => navigate("/login")}
+            onClick={() => navigate("/code-resend")}
             className="w-full text-center gap-2.5 py-3 border border-slate-100 rounded-sm cursor-pointer transition-all duration-500 ease-in-out hover:ring-blue-2 ring-1 flex items-center justify-center"
           >
             <p className="text-blue-clr cursor-pointer mb-0 transition-all duration-300 ease-in-out hover:underline">
-              Back to Sign In
+              Resend Code
             </p>
           </div>
           {/* <div className="w-full text-center gap-2.5 py-3 border border-slate-100 rounded-sm cursor-pointer transition-all duration-500 ease-in-out hover:ring-blue-2 flex items-center justify-center">
