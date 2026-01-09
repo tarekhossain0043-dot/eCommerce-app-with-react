828310c9a1e9dcf311598188050cdc889c0c4e8b
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2026-01-09 16:37:59 +0600

    vite.config file chunk issue for deployment on vercel

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,11 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            // This groups all node_modules into a single vendor chunk
-            return "vendor";
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
           }
         },
       },
