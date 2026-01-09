abfb3af3f011fe6ba7fedd8cf1b77217991086c4
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2026-01-09 16:33:18 +0600

    vite-config file issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // This groups all node_modules into a single vendor chunk
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
