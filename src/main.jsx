a557bcd47bfec551f8d2baf62731523153bc6e13
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2026-01-09 17:08:23 +0600

    added page on main branch

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/eCommerce-app-with-react">
       <App />
     </BrowserRouter>
   </Provider>
